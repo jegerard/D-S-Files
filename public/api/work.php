75812867a5a36ab19b0c344828b99e73a69fdcd9
Jenna Gerard <jegerard@indiana.edu>
2018-09-19 09:28:30 -0400

    More php

--- public/api/work.php
+++ public/api/work.php
@@ -1,5 +1,6 @@
 <?php
 
+echo '<pre>'
 $foo = "bar\n";
 $bar = 'bit\n';
 
@@ -10,3 +11,46 @@ echo $one = 'ONE';
 
 echo "The value is {$one}.\n";
 echo "The value is" . $one;
+
+class Animal
+{
+  public $name = 'Pig';
+}
+
+$wilber = new Animal();
+echo $wilber->name;
+
+$myARR = [
+
+  'first' =>'Tom',
+  'last' => 'Gregory'
+];
+
+$people = [
+
+  0 =>'Tom',
+  1 => 'Phil',
+  2 =>'Midge'
+];
+
+print_r($people);
+var_dump($myArr);
+
+/*
+$n = 0;
+
+if (isset($_GET['id'])){
+  $n = $_GET['id']
+}
+
+//This means the same thing:
+  $n = isset($_GET['id']) ? $_GET['id'] : 0;
+
+// Same thing in JS:
+  n = foo || default
+*/
+
+//This also means the same thing:
+  $n = $_GET['id'] ?? 0;
+
+echo '$n = ' . $n . "\n";
