b20e7fb89d501a95f7547750ad951729b8575347
Jenna Gerard <jegerard@indiana.edu>
2018-09-26 13:02:18 -0400

    team model

---
+++ app/models/Team.php
@@ -0,0 +1,27 @@
+<?php
+ class Team
+ {
+   public $id;
+   public $name;
+   public function __construct($data) {
+     //todo
+   }
+   public static function findAll() {
+     // 1. Connect to the database
+     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+     // 2. Prepare the query
+     $sql = 'SELECT * FROM Team';
+     $statement = $db->prepare($sql);
+     // 3. Run the query
+     $success = $statement->execute();
+     // 4. Handle the results
+     $arr = [];
+     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+       // 4.a. For each row, make a new work object
+       $theTeam =  new Team($row);
+       array_push($arr, $theTeam);
+     }
+     // 4.b. return the array of work objects
+     return $arr;
+   }
+ }
