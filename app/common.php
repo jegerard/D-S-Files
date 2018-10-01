01886e229abdc26a09b4e6eded97fdabf4624cfe
Jenna Gerard <jegerard@indiana.edu>
2018-10-01 08:44:37 -0400

    lkaj

--- app/common.php
+++ app/common.php
@@ -6,7 +6,7 @@ set_include_path (__DIR__);
 require 'environment.php';
 
 if ($_SERVER['REQUEST_METHOD']=='POST'
-&& stripos($_SERVER['CONTENT_TYPE'], 'application/json')) {
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json')!== false) {
   $_POST = json_decode(file_get_contents('php://input'), true);
 }
 
