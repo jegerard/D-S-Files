e09e135528d0635d5d2e25d70e77acfa67d0bc57
Jenna Gerard <jegerard@indiana.edu>
2018-10-01 08:34:06 -0400

    workPost

--- public/api/work.php
+++ public/api/work.php
@@ -1,11 +1,18 @@
 <?php
 require '../../app/common.php';
 
-// Get the taskID
-$taskId = $_GET['taskId'] ?? 0;
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+  REQUIRE 'workPost.php'
+}
 
+$taskId = intval($_GET['taskId'] ?? 0);
+if ($taskId < 1) {
+  throw new Exception('Invalid Task ID');
+}
 // 1. Go to the database and get all work associated with the $taskId
-$work = Work::getWorkByTaskId($taskId);
-// 2. Convert to JSON and print
-header('Content-type: application/json')
-echo json_encode($work);
+$workArr = Work::getWorkByTaskId($taskId);
+// 2. Convert to JSON
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
