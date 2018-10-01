761f69f64eeff7a8c181c6c50daa23fa5eee8851
Jenna Gerard <jegerard@indiana.edu>
2018-10-01 08:36:08 -0400

    common

--- app/common.php
+++ app/common.php
@@ -3,5 +3,8 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 require 'environment.php';
+
+if ($_SERVER['REQUEST_METHOD']=='POST'
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json'))
 /** MODELS **/
 require 'models/Work.php';
