dd75d2da38c6380d2c188d6d8e60fdce478a7967
Jenna Gerard <jegerard@indiana.edu>
2018-10-01 08:37:56 -0400

    again

--- app/common.php
+++ app/common.php
@@ -2,9 +2,14 @@
 // Change the working directory to this file.
 chdir(__DIR__);
 set_include_path (__DIR__);
+
 require 'environment.php';
 
 if ($_SERVER['REQUEST_METHOD']=='POST'
-&& stripos($_SERVER['CONTENT_TYPE'], 'application/json'))
+&& stripos($_SERVER['CONTENT_TYPE'], 'application/json')) {
+  $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
 /** MODELS **/
 require 'models/Work.php';
+require 'models/Team.php';
