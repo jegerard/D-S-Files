93db5d450be811790344ce9250cc76ac31d18af1
Jenna Gerard <jegerard@indiana.edu>
2018-10-09 19:45:34 -0400

    ?

--- app/models/Work.php
+++ app/models/Work.php
@@ -4,51 +4,34 @@ class Work
   public $id;
   public $task_id;
   public $team_id;
-  public $start_date;  //'YYYY-MM-DD'
+  public $start;  //'YYYY-MM-DD'
   public $stop;   //'YYYY-MM-DD', needs to be calculated
   public $hours;
   public $completion_estimate;
   public function __construct($row) {
-    $this->id = isset($row['id']) ? intval($row['id']) : null;
+    $this->id = intval($row['id']);
     $this->task_id = intval($row['task_id']);
     $this->team_id = intval($row['team_id']);
-    $this->start_date = $row['start_date'];
+    $this->start = $row['start_date'];
     $this->hours = floatval($row['hours']);
     // Calculate stop date
     $hours = floor($this->hours);
     $mins = intval(($this->hours - $hours) * 60); // Take advantage of one decimal place
     $interval = 'PT'. ($hours ? $hours.'H' : '') . ($mins ? $mins.'M' : '');
-    $date = new DateTime($this->start_date);
+    $date = new DateTime($this->start);
     $date->add(new DateInterval($interval));
     $this->stop = $date->format('Y-m-d H:i:s');
     $this->completion_estimate = intval($row['completion_estimate']);
   }
-  public function create() {
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    $sql = 'INSERT INTO Work (task_id, team_id, start_date, hours, completion_estimate)
-            VALUES (?, ?, ?, ?, ?)';
-    $statement = $db->prepare($sql);
-    $success = $statement->execute([
-      $this->task_id,
-      $this->team_id,
-      $this->start_date,
-      $this->hours,
-      $this->completion_estimate
-    ]);
-    if (!$success) {
-      // TODO: Better error handling
-      die('SQL error');
-    }
-    $this->id = $db->lastInsertId();
-  }
-  public static function getWorkByTaskId() {
+  public static function getWorkByTaskId(int $taskId) {
     // 1. Connect to the database
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
     // 2. Prepare the query
-    $sql = 'SELECT * FROM Work';
+    $sql = 'SELECT * FROM Work WHERE task_id = ?';
     $statement = $db->prepare($sql);
     // 3. Run the query
     $success = $statement->execute(
+        [$taskId]
     );
     // 4. Handle the results
     $arr = [];
@@ -59,4 +42,5 @@ class Work
     }
     return $arr;
   }
+
 }
