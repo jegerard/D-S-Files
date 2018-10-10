d199811bac8d9f88a346863a78f61899f6050fc3
Jenna Gerard <jegerard@indiana.edu>
2018-10-09 20:08:58 -0400

    dsfa

--- app/models/Work.php
+++ app/models/Work.php
@@ -29,10 +29,10 @@ class Work
     $sql = 'INSERT INTO Work (team_id, task_id, start_date, hours, completion_estimate) VALUES(?,?,?,?,?)';
     $statement = $db->prepare($sql);
     $success=$statement->execute([
-      $this->task_id;
-      $this->team_id;
-      $this->start;
-      $this->hours;
+      $this->team_id,
+      $this->task_id,
+      $this->start,
+      $this->hours,
       $this->completion_estimate
 
     ]
