e8f68bbfe61cc9a28a25acd2bbf368e63391346b
Jenna Gerard <jegerard@indiana.edu>
2018-10-10 11:01:57 -0400

    wertyui

--- public/api/workHours.php
+++ public/api/workHours.php
@@ -3,7 +3,7 @@ require '../../app/common.php';
 
 
 // 1. Go to the database and get all work associated with the $taskId
-$workArr = WorkHoursReport::fetchByProjectID($projectId);
+$workArr = WorkHoursReport::fetchByProjectID();
 // 2. Convert to JSON
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
 // 3. Print
